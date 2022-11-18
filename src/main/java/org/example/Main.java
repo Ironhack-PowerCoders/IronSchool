177b777734c4831b9c20a0026369ca0101ba1de1
Nuria Rodriguez <nur.rodga@gmail.com>
2022-11-18 17:57:23 +0100

    menu wip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,5 +16,44 @@ public class Main {
         7 user enter details of each student (scanner)
         8 User has to chose between ACTIONS:
         */
+
+
+        String schoolName = null;
+        boolean schoolNameIsCorrect = false;
+
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to our new APP to manage your school system \n ");
+
+
+        System.out.println(  "Please insert the name of your institution:");
+
+
+        schoolName = input.nextLine();
+
+        System.out.println("is " + schoolName + " correct? Type true or false to confirm");
+        //System.out.println("first try");
+        //schoolNameIsCorrect = input.nextLine();
+
+        while (schoolNameIsCorrect == false){
+            String response = input.nextLine();
+            if(response.equals("YES")){
+                schoolNameIsCorrect = true;
+            }else{
+
+            }
+
+            System.out.println("The name was wrong, please insert the new name for your institution");
+            schoolName = input.nextLine();
+            System.out.println("is " + schoolName + " correct? Type true or false to confirm");
+           // schoolNameIsCorrect = input.nextLine();
+        }
+        System.out.println("Welcome to " + schoolName + "\n \n How many teachers are in this institution?");
+
+
+
+
+
     }
 }
