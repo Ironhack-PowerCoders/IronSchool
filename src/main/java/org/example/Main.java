3d69bec4afdd3fb1fb6d3359470f7b36815ecbe8
daeneris88 <daeneris88@gmail.com>
2022-11-24 14:50:41 +0100

    changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,5 +16,51 @@ public class Main {
         7 user enter details of each student (scanner)
         8 User has to chose between ACTIONS:
         */
+
+        School school = new School(null);
+        boolean schoolNameIsCorrect = false;
+        Scanner input = new Scanner(System.in);
+        int numberCourses;
+        int numTeacher;
+        int counter = 0;
+
+        System.out.println("Welcome to our new APP to manage your school system \n ");
+        //System.out.println("first try");
+        //schoolNameIsCorrect = input.nextLine();
+        while (schoolNameIsCorrect == false){
+            String response = input.nextLine();
+            if(response.equalsIgnoreCase("true")){
+                schoolNameIsCorrect = true;
+            }else{
+                System.out.println(  "Please insert the name of your institution:");
+                school.setSchoolName(input.nextLine());
+                System.out.println("is " + school.getSchoolName() + " correct? Type true or false to confirm");
+            }
+        }
+
+        System.out.println("Welcome to " + school.getSchoolName() + "\n \n How many courses has this institution?");
+        numberCourses = Integer.parseInt(input.nextLine());
+        while(counter != numberCourses){
+            System.out.println(" ");
+            String name = input.nextLine();
+            System.out.println(" ");
+            Double price = Double.valueOf(input.nextLine());
+            System.out.println(" 1.morning \n 2.afternoon \n 3. night");
+            int time = Integer.parseInt(input.nextLine());
+            System.out.println(" ");
+            int year = Integer.parseInt(input.nextLine());
+            school.addCourse(new Course("name", 2000.00, "mañana", 1));
+            counter++;
+        }
+        counter = 0;
+
+
+        /*
+        System.out.println("Welcome to " + school.getSchoolName() + "\n \n How many teachers are in this institution?");
+        numTeacher = Integer.parseInt(input.nextLine());
+        while (counter != numTeacher){
+
+            school.addTeacher(new Teacher("name", 2000.00, ));
+        }*/
     }
 }
--- src/test/java/CommandsTest.java
+++ src/test/java/CommandsTest.java
@@ -1,6 +1,5 @@
 import org.example.*;
 import org.testng.annotations.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommandsTest {
